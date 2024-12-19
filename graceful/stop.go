40240cb9631634abeb9b1cf3bbd07fe9dcbae1bd
Eric Fixler <efixler@mozilla.com>
2024-12-19 14:54:06 -0500

    Return server shutdown error

--- graceful/stop.go
+++ graceful/stop.go
@@ -2,7 +2,6 @@ package graceful
 
 import (
 	"context"
-	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
@@ -14,10 +13,14 @@ import (
 // requests to clear, then call the passed cancel function.
 // This will let the requests finish before shutting down any other open
 // resources with the passed CancelFunc.
-// This function will block until server shutdown is complete
-func WaitForShutdown(s *http.Server, timeout time.Duration, cf ...context.CancelFunc) {
+// This function will block until server shutdown is complete.
+// If the server does not shut down within the timeout, it will return an error. The
+// error returned from server.Shutdown() will be returned here.
+func WaitForShutdown(s *http.Server, timeout time.Duration, cf ...context.CancelFunc) error {
 	waitForKill()
-	<-shutdownServer(s, timeout, cf...)
+	wchan, err := shutdownServer(s, timeout, cf...)
+	<-wchan
+	return err
 }
 
 func waitForKill() {
@@ -28,8 +31,7 @@ func waitForKill() {
 
 // Shutdown the server and then propagate the shutdown to a cancel function.
 // Caller should block on the returned channel.
-func shutdownServer(s *http.Server, timeout time.Duration, cf ...context.CancelFunc) chan bool {
-	slog.Info("server shutting down")
+func shutdownServer(s *http.Server, timeout time.Duration, cf ...context.CancelFunc) (chan bool, error) {
 	wchan := make(chan bool)
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	context.AfterFunc(ctx, func() {
@@ -41,9 +43,6 @@ func shutdownServer(s *http.Server, timeout time.Duration, cf ...context.CancelF
 		close(wchan)
 	})
 	defer cancel()
-	if err := s.Shutdown(ctx); err != nil {
-		slog.Error("server shutdown failed", "error", err)
-	}
-	slog.Info("server stopped")
-	return wchan
+	err := s.Shutdown(ctx)
+	return wchan, err
 }
